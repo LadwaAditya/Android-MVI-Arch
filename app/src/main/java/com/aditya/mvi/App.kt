218814c72a767d9d559bec41d4196c97de65fb04
Aditya <ladwa.aditya@gmail.com>
2018-02-11 23:23:43 +0530

    Feat: Added Missing dependency to data lib

--- app/src/main/java/com/aditya/mvi/App.kt
+++ app/src/main/java/com/aditya/mvi/App.kt
@@ -1,7 +1,35 @@
 package com.aditya.mvi
 
+import android.app.Activity
 import android.app.Application
+import com.aditya.mvi.injection.AppComponent
+import com.aditya.mvi.injection.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import javax.inject.Inject
 
-class App : Application() {
-
+class App : Application(), HasActivityInjector {
+    
+    lateinit var appComponent: AppComponent
+    
+    @Inject lateinit var activityDispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
+    
+    override fun activityInjector(): AndroidInjector<Activity> {
+        return activityDispatchingAndroidInjector
+    }
+    
+    override fun onCreate() {
+        super.onCreate()
+        createAppComponent()
+    }
+    
+    fun createAppComponent() {
+        appComponent = DaggerAppComponent.builder()
+            .application(this)
+            .remoteUrl("http://jsonplaceholder.typicode.com/")
+            .build()
+        appComponent.inject(this)
+    }
+    
 }
--- cache/src/main/java/in/epaylater/cache/entity/PostLocal.kt
+++ cache/src/main/java/in/epaylater/cache/entity/PostLocal.kt
@@ -1,9 +1,10 @@
 package `in`.epaylater.cache.entity
 
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
 
 @Entity(tableName = "post")
-data class PostLocal(val id: Int,
+data class PostLocal(@PrimaryKey val id: Int,
                      val userId: Int,
                      val title: String,
                      val body: String)
