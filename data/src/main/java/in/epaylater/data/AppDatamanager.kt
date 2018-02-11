7249fc40fb74124d57528faa5faee2d422f435ef
Aditya <ladwa.aditya@gmail.com>
2018-02-11 22:35:33 +0530

    Feat: Added data as android library module

---
+++ data/src/main/java/in/epaylater/data/AppDatamanager.kt
@@ -0,0 +1,3 @@
+package `in`.epaylater.data
+
+class AppDatamanager
--- data/src/main/java/in/epaylater/data/DataManager.kt
+++ data/src/main/java/in/epaylater/data/DataManager.kt
@@ -1,6 +1,6 @@
 package `in`.epaylater.data
 
+import `in`.epaylater.cache.DatabaseService
 import `in`.epaylater.remote.RemoteService
 
-
-interface DataManager : RemoteService
+interface DataManager : RemoteService, DatabaseService
