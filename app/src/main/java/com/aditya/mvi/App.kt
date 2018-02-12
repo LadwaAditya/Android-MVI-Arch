fdc9a80699cc06ce096b7c248767afde9cca10ed
Aditya <ladwa.aditya@gmail.com>
2018-02-12 20:48:38 +0530

    Build: Enabled build cache

--- app/src/main/java/com/aditya/mvi/App.kt
+++ app/src/main/java/com/aditya/mvi/App.kt
@@ -3,6 +3,7 @@ package com.aditya.mvi
 import android.app.Activity
 import android.app.Application
 import com.aditya.mvi.injection.AppComponent
+import com.aditya.mvi.injection.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
